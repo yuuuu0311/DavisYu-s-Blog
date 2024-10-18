7911195e3eb1fe4961d6314345edbbfb5676c1e0
yuuuu0311 <a26430803@gmail.com>
2024-10-19 00:00:40 +0800

    feat: install  vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
                     </main>
                     <Footer />
                 </div>
+                <Analytics />
             </body>
         </html>
     );
